27046bbcf8e2f10c4aeec1e865284e12a6ca4fcb
Reshma-Ghosh <reshmaghosh114@gmail.com>
2023-05-25 15:26:57 +0200

    TS-practice2

--- build/map.js
+++ build/map.js
@@ -1,12 +1,8 @@
+"use strict";
 // Exercise 1
 const number1 = [25, 45, 55, 77, 88, 99];
 // Using map() to square each number in the array
-const squareNumber: number[] = number1.map((num) => num * num);
-console.log(squareNumber);
-
 // Using map() to return a new array the double value of element
-
 // Exercise 2
-
 const strings = ["avengers", "captain america", "iron man", "black panther"];
 //   Using map() to return a new array that the first letter in the word would be uppercase
